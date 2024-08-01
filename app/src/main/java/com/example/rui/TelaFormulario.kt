6e11a34fda50d0194decb0caa2c96e3f12e63dd1
Luanvraft <luanbarroso1212@gmail.com>
2024-08-01 08:25:04 -0300

    Update telas form/inicial

--- app/src/main/java/com/example/rui/TelaFormulario.kt
+++ app/src/main/java/com/example/rui/TelaFormulario.kt
@@ -1,20 +1,38 @@
 package com.example.rui
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.rui.databinding.ActivityTelaFormularioBinding
 
 class TelaFormulario : AppCompatActivity() {
+
+    private lateinit var binding: ActivityTelaFormularioBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityTelaFormularioBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_tela_formulario)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        binding.CarneVermelha.setOnClickListener {
+            Ativar()
+        }
+        binding.CarneBranca.setOnClickListener {
+            Ativar()
+        }
+        binding.CarneVegetariana.setOnClickListener {
+            Ativar()
+        }
+    }
+    private fun Ativar(){
+        val caixa = binding.caixaDePergunta
+        caixa.visibility = View.VISIBLE
     }
 }
