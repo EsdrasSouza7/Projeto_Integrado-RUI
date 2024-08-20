b6df93528a3ff9217a84b818ed95ad1a092e8823
Esdras S <souzaesdras859@gmail.com>
2024-08-20 16:02:07 -0300

    validação Email

--- app/src/main/java/com/example/rui/MainActivity.kt
+++ app/src/main/java/com/example/rui/MainActivity.kt
@@ -36,7 +36,7 @@ class MainActivity : AppCompatActivity() {
             cardapio()
         }
         binding.BtnRespostas.setOnClickListener{
-
+            respostas()
         }
     }
     private fun cardapio(){
@@ -44,6 +44,7 @@ class MainActivity : AppCompatActivity() {
 
         binding.BtEntrar.isEnabled = false
         binding.BtAvaliar.isEnabled = false
+        binding.BtnRespostas.isEnabled = false
         binding.BtEntrar.setTextColor(Color.WHITE)
 
         Handler(Looper.getMainLooper()).postDelayed({ativarButoes()},500)
@@ -59,7 +60,8 @@ class MainActivity : AppCompatActivity() {
 
         binding.BtEntrar.isEnabled = false
         binding.BtAvaliar.isEnabled = false
-        binding.BtEntrar.setTextColor(Color.WHITE)
+        binding.BtnRespostas.isEnabled = false
+        binding.BtAvaliar.setTextColor(Color.WHITE)
 
         Handler(Looper.getMainLooper()).postDelayed({ativarButoes()},500)
         Handler(Looper.getMainLooper()).postDelayed({navegateTelaFormulario()},500)
@@ -74,14 +76,15 @@ class MainActivity : AppCompatActivity() {
 
         binding.BtEntrar.isEnabled = false
         binding.BtAvaliar.isEnabled = false
-        binding.BtEntrar.setTextColor(Color.WHITE)
+        binding.BtnRespostas.isEnabled = false
+        binding.BtnRespostas.setTextColor(Color.WHITE)
 
         Handler(Looper.getMainLooper()).postDelayed({ativarButoes()},500)
         Handler(Looper.getMainLooper()).postDelayed({navegateTelaRespostas()},500)
     }
 
     private fun navegateTelaRespostas() {
-        val intent = Intent(this, TelaFormulario::class.java)
+        val intent = Intent(this, TelaRespostas::class.java)
         startActivity(intent)
     }
 
@@ -89,7 +92,9 @@ class MainActivity : AppCompatActivity() {
         binding.progressBar.visibility = View.GONE
         binding.BtAvaliar.isEnabled = true
         binding.BtEntrar.isEnabled = true
+        binding.BtnRespostas.isEnabled = true
         binding.BtAvaliar.setTextColor(Color.BLACK)
         binding.BtEntrar.setTextColor(Color.BLACK)
+        binding.BtnRespostas.setTextColor(Color.BLACK)
     }
 }
